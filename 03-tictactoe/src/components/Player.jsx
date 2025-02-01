4dd6c516c9fce17f22d8436211800eaee16ab4d0
samwimmer <samwimmer00@gmail.com>
2025-02-01 15:20:13 -0700

    finished the header and player section

---
+++ 03-tictactoe/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [editing, setEditing] = useState(false);
+
+  function handleClick() {
+    setEditing((editing) => !editing); //this is the recommended way to change based off previous state
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value); //this event pointer comes automatically from the input element
+  }
+  return (
+    <li>
+      <span className="player">
+        {editing ? (
+          <input
+            text="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{editing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
