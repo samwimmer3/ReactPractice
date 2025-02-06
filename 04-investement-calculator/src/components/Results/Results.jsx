ab453705a9fb641b14c0d238454299aba469dbdb
samwimmer <122418125+samwimmer3@users.noreply.github.com>
2025-02-05 21:46:24 -0700

    finished investment app

--- 04-investement-calculator/src/components/Results/Results.jsx
+++ 04-investement-calculator/src/components/Results/Results.jsx
@@ -1,9 +1,43 @@
 import "./Results.css";
 
+import { calculateInvestmentResults } from "../../util/investment.js";
+import { formatter } from "../../util/investment.js";
+
 export default function Results({ userInput }) {
+  let outputData = calculateInvestmentResults(userInput);
+  let initialInvestment =
+    outputData[0].valueEndOfYear -
+    outputData[0].interest -
+    outputData[0].annualInvestment;
+
   return (
-    <div id="result">
-      <h1>Results</h1>
-    </div>
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Yearly Interest</th>
+          <th>Total Interest</th>
+          <th>Invested Capitol</th>
+        </tr>
+      </thead>
+      <tbody>
+        {outputData.map((data) => (
+          <tr key={data.year}>
+            <td>{data.year}</td>
+            <td>{formatter.format(data.valueEndOfYear)}</td>
+            <td>{formatter.format(data.interest)}</td>
+            <td>
+              {formatter.format(
+                data.valueEndOfYear -
+                  data.annualInvestment * data.year -
+                  initialInvestment
+              )}
+            </td>
+            <td>{formatter.format(data.valueEndOfYear - data.interest)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
