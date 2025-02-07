14d90eaef3008dcb4779e5c3e54cde84035a49d3
samwimmer <122418125+samwimmer3@users.noreply.github.com>
2025-02-06 20:53:12 -0700

    finished player component

--- 06-ref-and-portal/src/components/Player.jsx
+++ 06-ref-and-portal/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const inputRef = useRef();
+  const [name, setName] = useState("unknown entity");
+
+  function handleClick(event) {
+    if (inputRef.current.value === "") {
+      return;
+    }
+    setName(inputRef.current.value);
+    inputRef.current.value = "";
+  }
+  //using ref instead of extra state
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name}!</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={inputRef} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
